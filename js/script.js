089d7c2a163128dc8f23c0ab1c8f9a6b8d619526
Trotteriniku <stefanozidda3@gmail.com>
2023-11-07 15:17:39 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const {createApp}=Vue
 createApp({
     data(){
         return{
-
+            emails: [],
         }
     },
     methods:{
 
     },
     mounted(){
-        
+        for (let i = 0; i < 10; i++) {
+            axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((give) =>{
+            console.log(give.data.response);
+            this.emails.push(give.data.response);
+            console.log(this.emails);
+            })
+            
+        }
     }
-})
+}).mount('#app')
